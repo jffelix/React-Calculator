ac4eabe97d17d1c663498a51a0621f93b587ea9c
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-01-26 15:28:22 -0800

    Created progress on boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from "axios";
+
+const App = () => {
+
+    const [ mainTitle, setMainTitle ] = useState("");
+
+    const getTitle = () => {
+        
+    }
+
+    return (
+        <div>
+            <h1>Hello from App Component!</h1>
+        </div>
+    )
+
+}
+
+export default App
